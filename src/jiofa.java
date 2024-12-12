e57e11a7345abe7b26a288f963c160875405a738
BlackShima <blackshima02@gmail.com>
2024-12-12 21:59:52 +0700

    dasd

---
+++ src/jiofa.java
@@ -0,0 +1,10 @@
+public class jiofa {
+
+    public static void main(String[] args) {
+        System.out.println("Leave me alone");
+        System.out.println("Nope!");
+        System.out.println("โง่อ่ะ ไม่เข้าใจเลย");
+
+    }
+    
+}
