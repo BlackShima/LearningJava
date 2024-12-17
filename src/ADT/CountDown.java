4c014a814272fbcb5dd424a62b1e4dac9074e5e6
BlackShima <blackshima02@gmail.com>
2024-12-17 15:46:16 +0700

    learning

---
+++ src/ADT/CountDown.java
@@ -0,0 +1,17 @@
+package ADT;
+
+public class CountDown {
+    public static void main(String[] args){
+
+
+        int countDown = 20;
+
+        while (countDown>0){
+            System.out.println(countDown);
+            countDown--;
+        }
+        System.out.println("Countdown complete!");
+
+    }
+
+}
---
+++ src/ADT/CountUp.java
@@ -0,0 +1,18 @@
+package ADT;
+
+public class CountUp {
+    public static void main(String[] args){
+
+            int countUp = 0;
+            do{
+                System.out.println(countUp);
+                countUp++;
+        }
+            while(countUp<20);
+    }
+
+
+
+
+
+}
