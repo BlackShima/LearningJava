7d8e4f84715f82d52d52d470eef11ad7dab93ffe
BlackShima <blackshima02@gmail.com>
2024-12-17 15:48:24 +0700

    learning

--- src/ADT_101/CountDown.java
+++ src/ADT_101/CountDown.java
@@ -1,4 +1,4 @@
-package ADT;
+package ADT_101;
 
 public class CountDown {
     public static void main(String[] args){
--- src/ADT_101/CountUp.java
+++ src/ADT_101/CountUp.java
@@ -1,4 +1,4 @@
-package ADT;
+package ADT_101;
 
 public class CountUp {
     public static void main(String[] args){
