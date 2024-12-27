cc1c29e5ba590e503dc7677a56891d40cbbedbdb
BlackShima <blackshima02@gmail.com>
2024-12-28 02:56:06 +0700

    adt

---
+++ src/ADT_101/ADT_101/ProgrammingAssignment/sdf.java
@@ -0,0 +1,33 @@
+package ADT_101.ProgrammingAssignment;
+
+class Student{
+    private String cd;
+    private String id;
+    private String first;
+    private String last;
+
+    public Student(String cd,String id,String first,String last){
+        this.cd = cd;
+        this.id = id;
+        this.first = first;
+        this.last = last;
+    }
+    public String getCd(){
+        return cd;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public String getFirst(){
+        return first.strip();
+    }
+    public String getLast(){
+        return last;
+    }
+
+    @Override
+    public String toString() {
+        return id +" "+first+" "+last;
+    }
+}
