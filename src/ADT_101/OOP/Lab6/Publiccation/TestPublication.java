28356b598d8c59fedec34a8111136180110ae762
BlackShima <blackshima02@gmail.com>
2025-01-23 00:40:07 +0700

    asd

--- src/ADT_101/OOP/Lab6/Publiccation/TestPublication.java
+++ src/ADT_101/OOP/Lab6/Publiccation/TestPublication.java
@@ -10,7 +10,9 @@ public class TestPublication {
         publications[1] = new Magazine("Time Inc.", 50, 5.99, "Weekly Magazine", "Weekly.");
         publications[2] = new Book("HarperCollins", 300, 29.99, "Famous Book", "John Doe");
         publications[3] = new KidsMagazine("Disney", 30, 4.99, "Monthly Kids Magazine", "Monthly", "3-7 years");
-
+        publications[4] = new Magazine("DC Comic",30, 3.99, "Aquaman", "Weekly");
+        publications[5] = new Book("J.R.R Tolkien", 300, 300,"London Book","Hoobit");
+        publications[6] = new KidsMagazine("The Parents", 30, 4.99, "Kids Gangster", "Parents"," 3+ years");
         for (Publication publication : publications) {
             if (publication != null) {
                 publication.print();
